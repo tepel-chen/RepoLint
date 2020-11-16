d826594d90a3657478911eace7367091fff5f0ed
samfundev <samdinner@hotmail.com>
2020-11-16 15:37:24 -0500

    Check the parent folder of .svg files

--- Rules/ParentFolder.cs
+++ Rules/ParentFolder.cs
@@ -1,11 +1,18 @@
+using System.Linq;
+
 namespace RepoLint.Rules
 {
 	internal class ParentFolder : Rule
 	{
-		public ParentFolder() : base(".html .json .css") {}
+		public ParentFolder() : base(".html .json .css .svg") { }
+
+		readonly string[] repoFiles = new[] { "Bomb.svg", "BombSide.svg", "repo-logo-unminified.svg", "repo-logo.svg", "twitch.svg" };
 
 		protected override void Lint()
 		{
+			if (repoFiles.Contains(File.Name))
+				return;
+
 			var parentFolder = File.Directory.Name;
 
 			switch (File.Extension)
@@ -15,6 +22,16 @@ namespace RepoLint.Rules
 					break;
 				case ".json" when parentFolder != "JSON":
 					Report(".json files should be in the \"JSON\" folder.");
+					break;
+				case ".svg":
+					var parent = File.Directory.Parent;
+					while (parent != null && parent.Name != "img")
+						parent = parent.Parent;
+
+					if (parent != null)
+						break;
+
+					Report(".svg files should be in a folder that's inside the \"img\" folder.");
 					break;
 			}
 		}
