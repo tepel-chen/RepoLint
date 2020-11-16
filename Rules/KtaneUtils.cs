0b031336ef81163af7260145d5e1fde75e408429
samfundev <samdinner@hotmail.com>
2020-11-16 02:08:15 -0500

    Update the message for KtaneUtils rule

--- Rules/KtaneUtils.cs
+++ Rules/KtaneUtils.cs
@@ -7,7 +7,7 @@ namespace RepoLint.Rules
 		protected override void HTML(IDocument document)
 		{
 			if (document.QuerySelector("head script[src=\"js/ktane-utils.js\"]") == null)
-				Report("ktane-utils.js script should be included.");
+				Report("<script src=\"ktane-utils.js\"> should be included.");
 		}
 	}
 }
