0f8eb899d4a9d2dfe97adfc2440307798e623164
tepel-chen <bhead1793@i.softbank.jp>
2021-08-13 15:35:55 +0900

    Ja rules

---
+++ Rules/Ja.cs
@@ -0,0 +1,21 @@
+﻿using AngleSharp.Dom;
+using System.Text.RegularExpressions;
+
+namespace RepoLint.Rules
+{
+	internal class Ja : HTMLRule
+	{
+		protected override void HTML(IDocument document)
+		{
+      if(!Regex.IsMatch(File.Name, @"\(日本語 — .+\)")) {
+        return;
+      }
+      if(document.QuerySelector("html").GetAttribute("lang") != "ja") {
+        Report("<html>にlang=\"ja\"が必要です");
+      }
+      if(document.QuerySelector("link[href=\"css/font-japanese.css\"]") == null) {
+        Report("<link rel=\"stylesheet\" type=\"text/css\" href=\"css/font-*.css\">がありません");
+      }
+		}
+	}
+}
