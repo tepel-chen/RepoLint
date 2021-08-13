898cf7d90eed11c30570bb0af8603ce9fe4b09cf
tepel-chen <bhead1793@i.softbank.jp>
2021-08-13 14:51:37 +0900

    Check translated manual filenames

--- Rules/TranslatedFileName.cs
+++ Rules/TranslatedFileName.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 
 namespace RepoLint.Rules
@@ -9,28 +13,52 @@ namespace RepoLint.Rules
 
 		static TranslatedFileName()
 		{
-			moduleNames = Repository.Modules.ConvertAll(module => module.FileName ?? module.Name);
+			try
+			{
+				var httpClient = new HttpClient();
+				var response = httpClient.GetAsync("https://ktane.timwi.de/json/raw").Result;
+				if (!response.IsSuccessStatusCode)
+					return;
+
+				var jsonString = response.Content.ReadAsStringAsync().Result;
+				var modules = JsonSerializer.Deserialize<WebsiteJSON>(jsonString).KtaneModules;
+				moduleNames = modules.ConvertAll(module => module.FileName != null ? module.FileName : module.Name);
+			}
+			catch(Exception e)
+			{
+        Console.Error.WriteLine(e);
+				Console.Error.WriteLine("Failed to load the repository.");
+			}
 		}
 
 		static readonly List<string> moduleNames;
 
+		class WebsiteJSON
+		{
+			public List<KtaneModule> KtaneModules { get; set; }
+
+			public class KtaneModule
+			{
+				public string FileName { get; set; }
+				public string Name { get; set; }
+			}
+		}
+
 		protected override void Lint()
 		{
-			var filename = File.GetNameWithoutExtension();
+      var filename = File.GetNameWithoutExtension();
 
-			if (!Regex.IsMatch(filename, @"([A-Z\d❖][!-~]*) translated ") && !Regex.IsMatch(filename, @"\(.+ — .+\)")) return;
+      if(!Regex.IsMatch(filename, @"([A-Z\d❖][!-~]*) translated ") && !Regex.IsMatch(filename, @"\(.+ — .+\)") ) return;
 
 			var moduleName = Regex.Match(filename, @"^(.+) translated").Groups[1].ToString();
 
-			if (moduleNames != null && !moduleNames.Contains(moduleName))
-			{
-				Report($"No original module named \"{moduleName}\" found");
-			}
+			if (moduleNames != null && !moduleNames.Contains(moduleName)) {
+        Report($"No original module named \"{moduleName}\" found");
+      }
 
-			if (!Regex.IsMatch(filename, "^" + moduleName + @" translated \(.+ — .+\)( [^()]+)?( \([^()]+\))?$"))
-			{
-				Report("Invalid file name: expected \"(.+) translated \\(.+ — .+\\)( [^()]+)?( \\([^()]+\\))?.html\"");
-			}
+      if (!Regex.IsMatch(filename, @"^" + moduleName + @" translated \(.+ — .+\)( [^()]+)?( \([^()]+\))?$")) {
+         Report($"Invalid file name: expected \"(.+) translated \\(.+ — .+\\)( [^()]+)?( \\([^()]+\\))?.html\"");
+      }
 		}
 	}
 }
